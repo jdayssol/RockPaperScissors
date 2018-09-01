74b9581a3646ad152c82f562765c7817a01d3d2e
jdayssol <jdayssol@gmail.com>
2018-09-01 12:13:09 +0200

    Init project

---
+++ src/main/java/com/jdayssol/App.java
@@ -0,0 +1,13 @@
+package com.jdayssol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
