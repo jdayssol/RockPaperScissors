75a79b5cac739fa8dc30aa78d5813af21b67773b
jdayssol <jdayssol@effisoft-group.com>
2018-09-25 20:52:06 +0200

    Clean code the name of method

--- src/main/java/com/jdayssol/strategy/Motion.java
+++ src/main/java/com/jdayssol/strategy/Motion.java
@@ -1,7 +1,5 @@
 package com.jdayssol.strategy;
 
-import com.jdayssol.game.Statistic;
-
 /**
  * Motion is an enumeration which proposes 3 moves: rock / paper / scissors.
  * It also provides a function to return the result of the game between two motions.
@@ -10,24 +8,18 @@ public enum Motion {
 
 	Rock, Paper, Scissors;
 
-	/**
-	 * Return the result of the game
-	 * @param otherMotion : the motion of the other player.
-	 * @return 0 = tie, 1 = Player win, 2 = Other player win.
-	 */
 	public RoundResult isWinningAgainst(Motion otherMotion) {
 		RoundResult result = RoundResult.Tie;
-		//int result;
 		if (this == otherMotion)
 			result = RoundResult.Tie;
-		else if (otherMotion.winnerMotion() == this)
+		else if (otherMotion.winner() == this)
 			result = RoundResult.Win;
 		else
 			result = RoundResult.Lose;
 		return result;
 	}
 
-	public Motion winnerMotion() {
+	private Motion winner() {
 		Motion winnerMotion;
 		switch (this) {
 		case Rock:
@@ -40,7 +32,7 @@ public enum Motion {
 			winnerMotion = Motion.Rock;
 			break;
 		default:
-			throw new RuntimeException("Unexpected Motion switch result in  retrieveWinner()");
+			throw new RuntimeException("Unexpected Motion switch result in  winner()");
 		}
 		return winnerMotion;
 	}
